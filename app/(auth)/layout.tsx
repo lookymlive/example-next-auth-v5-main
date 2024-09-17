0df2c3bc78e863775c8869872720f044a10a84d1
Lookym <lookymlive@gmail.com>
2024-09-16 21:20:43 -0300

    Is very Good

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,6 +3,6 @@ const AuthLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div className="container">{children}</div>;
+  return <div className="grid place-items-center h-screen">{children}</div>;
 };
 export default AuthLayout;
